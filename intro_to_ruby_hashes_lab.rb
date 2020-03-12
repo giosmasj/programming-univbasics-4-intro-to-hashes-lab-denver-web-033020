256273f78e25bc5d164c113919f86df5f9beb6a2
James Giosmas <giosmasj@gmail.com>
2020-03-12 03:29:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,9 @@ def read_from_hash(hash, key)
   hash[key]
 end
 
-`def update_counting_hash(hash, key)
+def update_counting_hash(hash, key)
   if hash[key]
     hash[key] += 1
   else
     hash[key] = 1
-end`
+end
